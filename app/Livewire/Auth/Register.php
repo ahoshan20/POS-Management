b462499ca012254c2454cce253b896adb06f4a8d
Md Ahoshan Habib <mdahoshanhabib2003@gmail.com>
2025-08-03 01:04:27 +0600

    Layout setup

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire\Auth;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Register extends Component
 {
+
+    #[Layout('components.Backend.User.layouts.app')]
     public function render()
     {
         return view('livewire.auth.register');
